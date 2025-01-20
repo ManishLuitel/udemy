49706720b56a5925ec3a5a50d9047f04f7c15737
ManishLuitel <manishluitel1111@gmail.com>
2025-01-20 18:30:05 +0545

    program to generate random password

--- udemy.py
+++ udemy.py
@@ -428,4 +428,43 @@ print(("New lines can be created with a backslash and n.")
 # print(total)
 
 
+# '''Program that prints fizz if the number is divisible by 3 and print buzz if the number is divisible by 5 ,if the number is 
+# divisible by both 3 and 5 it prints fizz buzz'''
+
+# for number in range(1,101):
+#   if number % 3==0 and number % 5==0:
+#    print("fizz buzz")
+#   elif number % 3==0:
+#    print("fizz")
+#   elif number % 5==0:
+#    print("buzz")
+  
+#   else:
+#    print(number)
+  ''' Program to generate the random password '''
+
+# import random 
+# letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+# numbers = ['0','1','2','3','4','5','6','7','8','9']
+# symbols = ["!", "@", "#", "$", "%", "+", "&", "*", "(", ")"]
+
+# print("Welcome To The PyPassword Generator!")
+# nr_letters = int(input("How many letters would you like in your password?\n"))
+# nr_symbols = int(input("How many symbols would you like?\n"))
+# nr_numbers = int(input("How many number would you like?\n"))
+
+# password = []
+# for char in range(1,nr_letters + 1):
+#   password +=random.choice(letters)
+   
+# for char in range(1,nr_symbols +1):
+#   password +=random.choice(symbols)
+
+# for char in range(1,nr_numbers +1):
+#   password +=random.choice(numbers)
+# random.shuffle(password)
+# pasword = ""
+# for char in password:
+#   pasword += char
+# print(f"Your password is {pasword}")
 
